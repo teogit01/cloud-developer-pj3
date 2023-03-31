93397c84b239ac627e6700a73c3e0d1eb423d6dc
teogit01 <teob1605272@gmail.com>
2023-03-31 11:09:24 +0700

    initial

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=empl1
+export POSTGRES_PASSWORD=Pw0793303000
+export POSTGRES_HOST=empl1-instancedb.cebdyn4ab9eq.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=empl1-s3
+#export AWS_BUCKET=empl1-s3
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
