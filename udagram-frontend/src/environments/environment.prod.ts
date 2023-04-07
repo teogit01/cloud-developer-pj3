d96f3216134257a5597c7796cada710397e92661
teogit01 <teob1605272@gmail.com>
2023-04-07 13:34:31 +0700

    deploy phase

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a0104f9d339ea44ac8eac2d226e36fdd-793939790.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
