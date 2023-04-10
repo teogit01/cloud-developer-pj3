bf5fbe0cf8311574ca9b55fe9f9e5afc4b060c68
teogit01 <teob1605272@gmail.com>
2023-04-10 09:52:39 +0700

    fix bug

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,12 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=empl1
-export POSTGRES_PASSWORD=Pw0793303000
-export POSTGRES_HOST=empl1-instancedb.cebdyn4ab9eq.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=Pw0793303000#
+
+export POSTGRES_HOST=postgres.ckmgyzkrkwft.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
 export AWS_BUCKET=empl1-s3
-#export AWS_BUCKET=empl1-s3
+
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
