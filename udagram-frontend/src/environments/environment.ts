9ccd4a16ec7a20b3dda8538edf6c64b29ef70adc
teogit01 <teob1605272@gmail.com>
2023-04-10 16:48:36 +0700

    fix bug

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a555f491a58574694a16371da85b3426-1308725805.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
